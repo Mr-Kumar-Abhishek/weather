0ff9a443eb5c0203dd7a65f4b590923df6a71bce
Abhishek Kumar <mr.kumar.abhishek@email.com>
2016-12-08 16:56:18 +0530

    toggle temperature on click

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
+var cel;
+var far;
+var t_toggle = 0;
+
 if ("geolocation" in navigator) {
 	$("#weather-data").show();
 		navigator.geolocation.getCurrentPosition(function(position) {
-		load_weather(position.coords.latitude+','+position.coords.longitude);}); 
+		load_weather(position.coords.latitude+','+position.coords.longitude);});
 	//	load_weather("30.2676,-97.74298");
+		$("#temp").click(toggle_temp);
 }else {
 	$("#aww-snap").show();
 }
-
 
 function load_weather(location) {
   $.simpleWeather({
@@ -19,8 +23,10 @@ function load_weather(location) {
 		console.log(w.city);
 		console.log(w.currently);
 		console.log(w.region);
-		console.log(w.alt.temp); 
-		$("#temp").html("<p>" + w.temp + " &deg; C / " + w.alt.temp + " &deg; F </p>");
+		console.log(w.alt.temp);
+		cel = w.temp;
+		far = w.alt.temp; 
+		$("#temp").html("<p>" + w.temp + " &deg; C </p>");
 		$("#location").html("<p>" + w.city +  " / " + w.region + "</p>" ); 
 		$("#weather").html("<p>" + w.currently + "</p>");
     },
@@ -32,3 +38,15 @@ function load_weather(location) {
   });
 }
 
+function toggle_temp() {
+		console.log("toggle_temp");
+		if(t_toggle == 1) {
+			console.log("temprature to cel");
+			t_toggle = 0;
+			$("#temp").html("<p>" + cel + " &deg; C </p>");
+		}else { 
+			console.log("temprature to far");
+			t_toggle = 1;
+			$("#temp").html("<p>" + far + " &deg; F </p>");
+		}
+}
