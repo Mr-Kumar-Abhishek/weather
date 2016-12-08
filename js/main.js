a5da34ad661f56ee294949254301b66eaf6cb712
Abhishek Kumar <mr.kumar.abhishek@email.com>
2016-12-09 05:13:02 +0530

    include weather icons for different weather codes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var cel;
 var far;
+var icon;
+var icon_code;
 var t_toggle = 0;
 
 if ("geolocation" in navigator) {
@@ -25,7 +27,10 @@ function load_weather(location) {
 		console.log(w.region);
 		console.log(w.alt.temp);
 		cel = w.temp;
-		far = w.alt.temp; 
+		far = w.alt.temp;
+		icon_code = w.code;
+		icon = w_code_icon(parseInt(icon_code));
+		$("#weather-sym").html("<i class='wi " + icon + "'></i>"); 
 		$("#temp").html("<p>" + w.temp + " &deg; C </p>");
 		$("#location").html("<p>" + w.city +  " / " + w.region + "</p>" ); 
 		$("#weather").html("<p>" + w.currently + "</p>");
@@ -49,4 +54,57 @@ function toggle_temp() {
 			t_toggle = 1;
 			$("#temp").html("<p>" + far + " &deg; F </p>");
 		}
+}
+
+function w_code_icon(w_code){
+	switch(w_code){
+		case 0: return "wi-tornado";
+		case 1: return "wi-storm-warning";
+		case 2: return "wi-hurricane";
+		case 3: return "wi-thunderstorm";
+		case 4: return "wi-storm-showers";
+		case 5: return "wi-rain-mix";
+		case 6: return "wi-rain-mix";
+		case 7: return "wi-sleet";
+		case 8: 
+		case 9: return "wi-sprinkle";
+		case 10: 
+		case 11:
+		case 12: return "wi-showers";
+		case 13: 
+		case 14: return "wi-snow";
+		case 15: return "wi-snow-wind";
+		case 16: return "wi-snow";
+		case 17: return "wi-hail";
+		case 18: return "wi-sleet";
+		case 19: return "wi-dust";
+		case 20: return "wi-fog";
+		case 21: return "wi-day-haze";
+		case 22: return "wi-smoke";
+		case 23: return "wi-cloudy-gusts";
+		case 24: return "wi-cloudy-windy";
+		case 25: return "wi-snowflake-cold";
+		case 26: return "wi-cloudy";
+		case 27: return "wi-night-alt-cloudy";
+		case 28: return "wi-day-cloudy";
+		case 29: return "wi-night-partly-cloudy";
+		case 30: return "wi-day-cloudy-high";
+		case 31: return "wi-night-clear";
+		case 32: return "wi-day-sunny";
+		case 33: return "wi-stars";
+		case 34: return "wi-day-sunny";
+		case 35: return "wi-rain-mix";
+		case 36: return "wi-hot";
+		case 37: return "wi-thunderstorm";
+		case 38: return 
+		case 39: return "wi-storm-showers";
+		case 40: return "wi-showers";
+		case 41: return "wi-snow-wind";
+		case 42: return "wi-rain-mix";
+		case 43: return "wi-snow-wind";
+		case 44: return "wi-cloud";
+		case 46: return "wi-rain-mix";
+		case 47: return "wi-thunderstorm";
+		case 3200: return "wi-na";
+	}
 }
